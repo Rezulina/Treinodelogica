230f9dd225b72250458f4c7e81aaf66ecc4ca05b
Rezulina <rezulina.costa@gmail.com>
2022-12-11 01:11:34 -0300

    treinando lógica

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//criando variável nome completo
+let nomecompleto = "Rezulina da Costa Silva";
+console.log(nomecompleto);
+
+//criando variáveis numéricas
+let numeroA = "5";
+let numeroB = "10";
+let resultado = numeroA*numeroB;
+
+console.log(resultado);
+
+//criando uma função
+function saudação() 
+   
